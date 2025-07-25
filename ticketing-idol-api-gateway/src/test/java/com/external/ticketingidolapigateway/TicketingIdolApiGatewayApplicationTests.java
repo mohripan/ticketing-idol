d0f24f1bd3efae62e0d6bd463906965423534acc
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2025-07-25 22:58:31 +0700

    feat: Initial Api Gateway

---
+++ ticketing-idol-api-gateway/src/test/java/com/external/ticketingidolapigateway/TicketingIdolApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.external.ticketingidolapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingIdolApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
