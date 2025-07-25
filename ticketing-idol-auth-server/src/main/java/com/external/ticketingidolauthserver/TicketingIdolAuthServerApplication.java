dec4216c1bfa4c512274b93d35fcd21e9bacd00f
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2025-07-25 23:04:28 +0700

    feat: Initial Auth Server

---
+++ ticketing-idol-auth-server/src/main/java/com/external/ticketingidolauthserver/TicketingIdolAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.external.ticketingidolauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingIdolAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingIdolAuthServerApplication.class, args);
+    }
+
+}
---
+++ ticketing-idol-auth-server/src/test/java/com/external/ticketingidolauthserver/TicketingIdolAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.external.ticketingidolauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingIdolAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
